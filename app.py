290a473fdbb00e9bff98c9039d8fc8c361624220
AfzalSurti <surtishabana35@gmail.com>
2026-02-25 10:53:32 +0530

    Whole Moduler Pipeline is Completed

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from src.data_loader import load_all_documents
 from src.embedding import EmbeddingPipeline
 from src.vectorestore import FaissVectorStore
+from src.search import RAGSearch
 
 if __name__=="__main__":
-    docs=load_all_documents("data")
+    #docs=load_all_documents("data")
     store=FaissVectorStore("faiss_store")
-    store.build_from_documents(docs)
-    print("Vector store built successfully.")
+    #store.build_from_documents(docs)
+    store.load()
+    #print(store.query("What is attention mechanism?", top_k=3))
+
+    rag_search = RAGSearch()
+    summary = rag_search.search_and_summarize("What is attention mechanism?", top_k=3)
+    print("Summary:", summary)
+
+    
 
     
