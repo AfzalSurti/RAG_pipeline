3b801765c39a504a021b685bd3345714d89e6b1d
AfzalSurti <surtishabana35@gmail.com>
2026-02-25 10:36:10 +0530

    the whole pipeline was built ina  one file now you need to call one method and everything works

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from src.data_loader import load_all_documents
 from src.embedding import EmbeddingPipeline
-
+from src.vectorestore import FaissVectorStore
 
 if __name__=="__main__":
     docs=load_all_documents("data")
-    chunks = EmbeddingPipeline().chunk_documents(docs)
-    chunks_vectors=EmbeddingPipeline().embed_chunks(chunks)
-    print(chunks_vectors)
+    store=FaissVectorStore("faiss_store")
+    store.build_from_documents(docs)
+    print("Vector store built successfully.")
 
     
